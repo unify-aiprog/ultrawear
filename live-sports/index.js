4cc86d0e9d90d0d06a5288800c771088cd4d8d69
unify-aiprog <unifyapp340@gmail.com>
2026-09-01 20:48:59 +0100

    feat(live-sports): export adapter and event page primitives

--- live-sports/index.js
+++ live-sports/index.js
@@ -2,3 +2,6 @@ export * from './events.js';
 export * from './community.js';
 export * from './follow.js';
 export * from './location.js';
+export * from './adapter.js';
+export * from './freshness.js';
+export * from './event-page.js';
