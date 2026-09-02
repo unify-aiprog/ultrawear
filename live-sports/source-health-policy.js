57a35d7ded779393acb5dfef0fd37ae9e75480c0
unify-aiprog <unifyapp340@gmail.com>
2026-09-02 12:34:32 +0100

    feat: expose source health policy facade

---
+++ live-sports/source-health-policy.js
@@ -0,0 +1,2 @@
+/** Public policy facade for continuous source health. */
+export { DEFAULT_FRESHNESS, freshnessWindow, classifySourceHealth, createSourceHealthTracker, nextPollDelay, isSourceStatus } from './source-health.js';
