188b08b74155f493eb5f2fe268c99632ebfa69d2
unify-aiprog <unifyapp340@gmail.com>
2026-09-03 18:58:25 +0100

    feat: add visual experience route

---
+++ app/experience/page.tsx
@@ -0,0 +1,8 @@
+import { UltraVisualEngine } from '@/components/ultra-visual-engine';
+import '../visual-engine.css';
+
+export const metadata = { title: 'Experience' };
+
+export default function ExperiencePage() {
+  return <UltraVisualEngine />;
+}
