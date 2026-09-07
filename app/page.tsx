298bba7cf36f5dcb1732d06deddbec5ada1fc9c7
unify-aiprog <unifyapp340@gmail.com>
2026-09-07 13:23:03 +0100

    feat: add quests to the living sports home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { PremiumHome } from '@/components/premium-home';
 import { LiveExperience } from '@/components/live-experience';
 import { SportsIdentity } from '@/components/sports-identity';
+import { QuestBoard } from '@/components/quest-board';
 import { CommunityHub } from '@/components/community-hub';
 import './premium-home.css';
 import './live-world.css';
 import './community.css';
+import './participation.css';
 
 export const revalidate = 0;
 
@@ -13,6 +15,7 @@ export default function HomePage() {
     <PremiumHome />
     <LiveExperience />
     <SportsIdentity />
+    <QuestBoard />
     <CommunityHub />
   </>;
 }
