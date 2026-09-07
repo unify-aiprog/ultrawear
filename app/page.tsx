4eb5728f05f00077801eb717d895003221c586b7
unify-aiprog <unifyapp340@gmail.com>
2026-09-07 13:22:41 +0100

    feat: surface identity and community loops on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { PremiumHome } from '@/components/premium-home';
 import { LiveExperience } from '@/components/live-experience';
+import { SportsIdentity } from '@/components/sports-identity';
+import { CommunityHub } from '@/components/community-hub';
 import './premium-home.css';
 import './live-world.css';
+import './community.css';
 
 export const revalidate = 0;
 
@@ -9,5 +12,7 @@ export default function HomePage() {
   return <>
     <PremiumHome />
     <LiveExperience />
+    <SportsIdentity />
+    <CommunityHub />
   </>;
 }
